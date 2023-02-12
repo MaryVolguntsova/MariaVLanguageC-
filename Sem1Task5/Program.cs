a8f168373942d517a52982e4c987974a7ef50370
Mary <volmarygun@gmail.com>
2023-02-12 11:17:43 +0300

    задача с числами от -N до N

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает одно число N, 
+// а на выходе показывает все целые число от -N до N.
+
+Console.Write("Введите любое целое число: ");
+
+string? lineN = Console.ReadLine();
+if(lineN != null)
+{
+    int N = int.Parse(lineN);
+    string res = string.Empty;
+    int negN = N*(-1);
+    while(negN <= N)
+    {
+        res = res + negN + "  ";
+        negN = negN + 1;
+    }
+
+Console.WriteLine(res);
+}
+
+// negN = negN++ (тоже, что и +1)
