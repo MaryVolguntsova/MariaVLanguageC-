194c966665e0aaeb95d64c47a2b8947fd6d938c9
Maria Volguntsova <volmarygun@gmail.com>
2023-02-22 17:58:50 +0300

    домашнее задание

--- Sem3Task23/Program.cs
+++ Sem3Task23/Program.cs
@@ -6,11 +6,15 @@ int ReadData(string msg)
     return int.Parse(Console.ReadLine()??"0");
 }
 
+//Вводим метод возведения в степень через математическую функцию
 string LineBuilder(int num, int pow)
 {
+    // вводим переменную где будет хранится результат метода, можно написать 
     string res = String.Empty;
+    // перебираем числа от 1 до введенного и все возводим в куб
     for(int i = 1; i <= num; i++)
     {
+        // записываем в строку результата
         res = res + Math.Pow(i, pow) + " ";
     }
 
@@ -18,4 +22,5 @@ string LineBuilder(int num, int pow)
 }
 int num = ReadData("Введите число: ");
 Console.WriteLine(LineBuilder(num,1));
+// Степень обозначается переменной int pow и тут в данных мы указываем ее значение, а num у нас в переменной
 Console.WriteLine(LineBuilder(num,3));
