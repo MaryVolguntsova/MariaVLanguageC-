7d8fcd57f12e63151c7a37777c5eadb8bb49e62c
Mary <volmarygun@gmail.com>
2023-02-22 23:50:53 +0300

    Решение 28 и 30 задачи

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №28
+// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+using System.Numerics;
+int ReadData(string line)
+{
+    // Выводим сообщение(что хотим получить)
+    Console.Write(line);
+    // сразу возвращаем значение, не заводя его в переменную 
+    return Convert.ToInt32(Console.ReadLine()??"0");
+}
+void PrintData(string line)
+{
+    Console.WriteLine(line);
+}
+BigInteger FactorialCalc(int num)
+{
+    BigInteger fact = 1;
+    for( int i = 2; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int num = ReadData("Введите число: ");
+PrintData("Факториал заданного числа: " +FactorialCalc(num));
---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,33 @@
+﻿// 
+int ReadData(string line)
+{
+    // Выводим сообщение(что хотим получить)
+    Console.Write(line);
+    // сразу возвращаем значение, не заводя его в переменную 
+    return Convert.ToInt32(Console.ReadLine()??"0");
+}
+
+void Print1DArray(int[]arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");    
+    }
+    Console.Write(arr[arr.Length-1]);
+    Console.WriteLine("]");
+}
+int[] Gen1DArray(int len, int min, int max)
+{
+    int[] arr = new int[len];
+    for(int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+int len = ReadData("Введие количество элементов массива: ");
+int min = ReadData("Введите минимальное значение заполнения массива ");
+int max = ReadData("Введите максимальное значение заполнения массива ");
+
+Print1DArray(Gen1DArray(len, min, max));
