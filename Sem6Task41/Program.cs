59f3c492f191218398f8bbc6e6e3fb23e647f93a
Mary <volmarygun@gmail.com>
2023-03-04 17:11:44 +0300

    Домашнее задание после 6 семинара

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// *Пользователь вводит число нажатий, затем программа следит за нажатиями и выдает сколько чисел больше 0 было введено.
+int ReadData(string line)
+{
+    // Выводим сообщение(что хотим получить)
+    Console.Write(line);
+    // сразу возвращаем значение, не заводя его в переменную 
+    return Convert.ToInt32(Console.ReadLine()??"0");
+}
+void PrintData(string line)
+{
+    Console.WriteLine(line);
+}
+int CountNum(int numM)
+{
+    int res = 0;
+    for(int i = 0; i < numM; i++)
+    {
+        if((ReadData("Введите число: ")) > 0)
+        {res++;    
+        }
+    }
+    return res;
+}
+int nums = ReadData("Сколько чисел вы хотите проверить?: ");
+int res = CountNum(nums);
+PrintData("Среди введенных чисел больше нуля: "+res);
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Заданы уравнения y = k1 * x + b1, y = k2 * x + b2, введите значения b1, k1, b2 и k2 через пробел для поиска точки пересечения прямых: ");
+int[] n = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+
+if(n.Length<=4)
+{
+double b1 = n[0];
+double k1 = n[1];
+double b2 = n[2];
+double k2 = n[3];
+double x = (b2 - b1)/(k1-k2);
+double y = k1*x + b1;
+Console.Write($"Координаты точки перечения - ({x},{y})");
+}
+else Console.WriteLine("Вы ввели слишком много чисел, попробуйте еще раз.");
