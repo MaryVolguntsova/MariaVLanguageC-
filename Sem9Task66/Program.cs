d1142c7a733d92587332e4e5152da104775ade4e
Mary <volmarygun@gmail.com>
2023-03-14 23:17:58 +0300

    Домашнее задание после 9 семинара

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int ReadData(string line)
+{
+    // Выводим сообщение(что хотим получить)
+    Console.Write(line);
+    // сразу возвращаем значение, не заводя его в переменную 
+    return Convert.ToInt32(Console.ReadLine()??"0");
+}
+int RecSumDigitMN(int m, int n)
+{
+    // Вводим точку остановки рекурсии;
+    if(m >= n) return m;
+    else return m + RecSumDigitMN(m+1, n);
+}
+int one = ReadData("Введите число M: ");
+int two = ReadData("Введите число N: ");
+// Проверка на дурака
+int res = one < two?RecSumDigitMN(one, two):RecSumDigitMN(two, one);
+
+Console.WriteLine(res);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 68
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int ReadData(string line)
+{
+    // Выводим сообщение(что хотим получить)
+    Console.Write(line);
+    // сразу возвращаем значение, не заводя его в переменную 
+    return Convert.ToInt32(Console.ReadLine()??"0");
+}
+
+// описание функции Аккермана найдено в интернете и прописаны все условия ее выполнения
+int Ackermann(int m, int n)
+{
+ if (m == 0)
+{
+ return n + 1;
+}
+else if (n == 0)
+    {
+    return Ackermann(m - 1, 1);
+    }
+    else
+    {
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+
+int one = ReadData("Введите число M: ");
+int two = ReadData("Введите число N: ");
+// Проверка на дурака
+int res = one < two?Ackermann(one, two):Ackermann(two, one);
+
+Console.WriteLine(res);
+
+
+
